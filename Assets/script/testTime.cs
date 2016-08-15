bf16d92c83d49ef82d4392bdc912de2545d6b717
lenovezhou <271130940@qq.com>
2016-08-15 22:31:03 +0800

    Trail Renderer

--- Assets/script/testTime.cs
+++ Assets/script/testTime.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
-public class testTime : MonoBehaviour {
+public class testTime : MonoBehaviour,IDragHandler,IPointerEnterHandler {
+
+	private bool isenter=false;
+
+	public void OnPointerEnter (PointerEventData eventData)
+	{
+		isenter = true;
+	}
+
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		float a = Input.GetAxis ("Mouse X")*21f;
+		float b = Input.GetAxis ("Mouse Y")*21f;
+
+		GetComponent<RectTransform> ().localPosition += new Vector3 (a,b,0);
+	}
+
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
