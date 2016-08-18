28fd87d10f4bb47f277cddf10c8db5d1599fa519
lenovezhou <271130940@qq.com>
2016-08-18 20:56:57 +0800

    Tidy

--- Assets/script/Singoleton_invoke/InvokeSigoleton.cs
+++ Assets/script/Singoleton_invoke/InvokeSigoleton.cs
@@ -3,11 +3,8 @@ using System.Collections;
 
 public class InvokeSigoleton : Singleton<InvokeSigoleton>{
 	
-	
 	public override void Awake ()
 	{
-		Debug.Log ("itselfAwake____begine");
 		base.Awake ();
-		Debug.Log ("afterParentAwake");
 	}
 }
---
+++ Assets/script/test.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class test : MonoBehaviour {
+
+    public delegate void dele();
+    public dele d_dele;
+    private Button buttons;
+    private GameObject ALL;
+
+    void Start() 
+    {
+        StartCoroutine(LoadBoundle(d_dele));
+        buttons=GetComponent<Button>();
+        buttons.onClick.AddListener(Oncklik);
+    }
+
+    void Oncklik() 
+    {
+        d_dele();
+    }
+    public IEnumerator LoadBoundle(dele tempdele) 
+    {
+
+        WWW w = new WWW(Application.dataPath + "/StreamingAssets/ALL.assetbundle");
+        yield return w;
+        Debug.LogError(w.error+"downerro"+w.isDone);
+        tempdele += ChangeStates;
+        ALL = (GameObject)Instantiate(w.assetBundle.mainAsset);
+        ALL.transform.localPosition = Vector3.zero;
+    }
+    void ChangeStates() 
+    {
+        ALL.transform.LookAt(Camera.main.transform.position);
+        float a = 360f;
+        while (a > 0) 
+        {
+            a += 0.1f;
+            ALL.transform.eulerAngles = new Vector3(0,a,0);
+        }
+    }
+    void OnDisable() 
+    {
+        d_dele -= ChangeStates;
+    }
+}
